c7e26ea3c543cac043c1a5cbcbd5bd6cd92b01e8
Juraci Paixão Kröhling <juraci@kroehling.de>
2021-12-14 15:31:17 +0100

    Add otelcol-contrib release
    Fixes #37
    
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ YQ_VERSION ?= 4.11.1
 YQ_DIR ?= ${OTELCOL_BUILDER_DIR}
 YQ ?= ${YQ_DIR}/yq
 
-DISTRIBUTIONS ?= "otelcol"
+DISTRIBUTIONS ?= "otelcol,otelcol-contrib"
 
 ci: check build
 check: ensure-goreleaser-up-to-date
--- scripts/build.sh
+++ scripts/build.sh
@@ -18,7 +18,7 @@ done
 
 if [ -z $distributions ]; then
     echo "List of distributions to build not provided. Use '-d' to specify the names of the distributions to build. Ex.:"
-    echo "$0 -d opentelemetry-collector"
+    echo "$0 -d otelcol"
     exit 1
 fi
 
