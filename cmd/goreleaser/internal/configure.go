1f41f55213c6cf9f7c7a3fcd7ca942311309de1c
Douglas Camata <159076+douglascamata@users.noreply.github.com>
2025-01-13 14:50:30 +0100

    Fix build flag templates

--- cmd/goreleaser/internal/configure.go
+++ cmd/goreleaser/internal/configure.go
@@ -538,8 +538,8 @@ func dockerImageWithOS(dist, os, arch string, opts containerImageOptions) config
 		imageConfig.Goarm = opts.armVersion
 	}
 	if os == "windows" {
-		imageConfig.ImageTemplates = slices.Insert(
-			imageConfig.ImageTemplates, 0, fmt.Sprintf("--build-arg WIN_VERSION=%s", opts.winVersion),
+		imageConfig.BuildFlagTemplates = slices.Insert(
+			imageConfig.BuildFlagTemplates, 0, fmt.Sprintf("--build-arg WIN_VERSION=%s", opts.winVersion),
 		)
 	}
 	return imageConfig
