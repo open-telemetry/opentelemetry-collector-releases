16e5ac4ef376cea37789efd5ff3e509b189a5dc1
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2025-08-25 09:12:06 +0200

    revert windows skips

--- cmd/goreleaser/internal/configure.go
+++ cmd/goreleaser/internal/configure.go
@@ -799,6 +799,8 @@ func dockerImageWithOS(dist, os, arch string, opts containerImageOptions) config
 		)
 		imageConfig.Dockerfile = "Windows.dockerfile"
 		imageConfig.Use = "docker"
+		imageConfig.SkipBuild = "{{ not (eq .Runtime.Goos \"windows\") }}"
+		imageConfig.SkipPush = "{{ not (eq .Runtime.Goos \"windows\") }}"
 	}
 	return imageConfig
 }
