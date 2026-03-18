9a5bf5241c6ecaba5eb89e1d5caaf3f66ea7db98
Antoine Toulme <atoulme@splunk.com>
2026-03-18 09:54:57 -0700

    [all] add aix/ppc64 binaries and images

--- cmd/goreleaser/internal/distro_k8s.go
+++ cmd/goreleaser/internal/distro_k8s.go
@@ -9,10 +9,12 @@ var (
 	// k8s distro
 	k8sDist = newDistributionBuilder(k8sDistro).withConfigFunc(func(d *distribution) {
 		d.BuildConfigs = []buildConfig{
+			&fullBuildConfig{TargetOS: "aix", TargetArch: aixArchs, BuildDir: defaultBuildDir},
 			&fullBuildConfig{TargetOS: "linux", TargetArch: k8sArchs, BuildDir: defaultBuildDir, Ppc64Version: []string{"power8"}},
 			&fullBuildConfig{TargetOS: "windows", TargetArch: winContainerArchs, BuildDir: defaultBuildDir},
 		}
 		d.ContainerImages = slices.Concat(
+			newContainerImages(d.Name, "aix", aixArchs, containerImageOptions{}),
 			newContainerImages(d.Name, "linux", k8sArchs, containerImageOptions{armVersion: "7"}),
 			newContainerImages(d.Name, "windows", winContainerArchs, containerImageOptions{winVersion: "2019"}),
 			newContainerImages(d.Name, "windows", winContainerArchs, containerImageOptions{winVersion: "2022"}),
